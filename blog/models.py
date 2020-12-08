79ee7d043fff1231ff39c5a1c8a3765ad720e2a9
maaksa <milosmilos@ymail.com>
2020-12-08 14:04:45 +0100

    Kreirani templejti about, post_lists, post_detail

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,8 @@ from django.urls import reverse
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField();
-    create_date = models.DateTimeField(default=timezone.now())
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
@@ -26,7 +26,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE())
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
