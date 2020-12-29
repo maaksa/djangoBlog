294f464b12f6d7f017fea8d4f0b6769c2ee455eb
maaksa <milosmilos@ymail.com>
2020-12-29 12:56:55 +0100

    final

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from django.urls import reverse
 
 
-# Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
--- blog/views.py
+++ blog/views.py
@@ -10,10 +10,8 @@ from django.views.generic import (TemplateView, ListView,
                                   UpdateView, DeleteView)
 
 
-# Create your views here.
-
 class AboutView(TemplateView):
-    template_name = "about.html"
+    template_name = "blog/about.html"
 
 
 class PostListView(ListView):
